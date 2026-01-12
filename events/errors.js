8a33a2340cc8dbbdcb1e9303426574e80ad399ab
mudau-t <mulu20111104@gmail.com>
2026-01-12 09:09:54 +0200

    Create errors.js

---
+++ events/errors.js
@@ -0,0 +1,13 @@
+/**
+ * Event: error
+ * Handles unexpected errors at the global level.
+ */
+const { logError } = require("../utils/logger");
+
+module.exports = {
+  name: "error",
+  execute(error) {
+    logError(`❌ A global error occurred: ${error}`);
+  },
+};
+ 
